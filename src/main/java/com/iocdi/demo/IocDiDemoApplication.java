a08601fcaeb61e8636642b5bced0e809c10850cc
Oleksiy Blavat <oleksiy.blavat@amobee.com>
2022-05-25 18:05:43 +0200

    initial commit :eggplant:

---
+++ src/main/java/com/iocdi/demo/IocDiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.iocdi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IocDiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IocDiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iocdi/demo/IocDiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iocdi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocDiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
